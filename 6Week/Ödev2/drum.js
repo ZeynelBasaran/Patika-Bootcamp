666bc05e4f57af85a8daf4891bb60f53ee16c17e
Zeynel <zeynelbasaran@hotmail.com>
2024-07-11 02:39:33 +0300

    ödev

--- 6Week/Ödev2/drum.js
+++ 6Week/Ödev2/drum.js
@@ -1,29 +1,10 @@
-/*
-const audio = ["boom","clap","drum","hihat","kick","openhat","ride","snare","tink","wav"]
-
-audio.forEach(sound => {
-    const btn = document.createElement(`button`)
-    btn.classList.add(`btn`)
-    btn.innerText = sound
-    body.appendChild(btn)
-    
-    btn.addEventListener("click",(a) => {
-        stopSongs()
-        document.getElementById(sound).play()
-
-    })
-
-});
-*/
-
 window.addEventListener("keyup",(e)=>{
     const val = document.querySelectorAll("ul>li")
-    
-   
     val.forEach((el)=>{
         let text = el.innerText.toLowerCase()
         
         if(text==e.key){
+            stopSongs()
             let sounds = `a${el.id}`
             document.getElementById(sounds).play()
             el.classList.add("rgba")
@@ -33,6 +14,14 @@ window.addEventListener("keyup",(e)=>{
 
 
         }
+        function stopSongs(){
+            const audi = document.querySelectorAll("audio")
+            audi.forEach((adios)=>{
+                adios.pause()
+                adios.currentTime = 0;
+            })
+        }
+        
     })
 })
 
